eb7ff288286c692ab84f0a23bc5d9b52a8636243
Andrew Lam <lam00139@morris.umn.edu>
2024-02-10 23:27:25 -0600

    Adding two path for todos and todos/:id to routes:Routes
    Co-authored-by: TyBeas <beasl110@morris.umn.edu>

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UserListComponent } from './users/user-list.component';
 import { UserProfileComponent } from './users/user-profile.component';
+import { TodoListComponent } from './todos/todo-list.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent, title: 'Home'},
   {path: 'users', component: UserListComponent, title: 'Users'},
-  {path: 'users/:id', component: UserProfileComponent, title: 'User Profile'}
+  {path: 'users/:id', component: UserProfileComponent, title: 'User Profile'},
+  {path: 'todos', component: TodoListComponent, title: 'Todos'},
+  {path: 'todos/:id', component: TodoListComponent, title: 'Todo Profile'}
 ];
 
 @NgModule({
