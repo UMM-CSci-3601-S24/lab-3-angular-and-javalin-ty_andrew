72222eb08320be52878be07857497ffb8f9820ad
Andrew Lam <lam00139@morris.umn.edu>
2024-02-10 23:08:49 -0600

    Create todo-list.components.ts and spec.ts in todos.
    Co-authored-by: TyBeas
    <beasl110@morris.umn.edu>

---
+++ client/src/app/todos/todo-list.component.spec.ts
@@ -0,0 +1,130 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs';
+import { MockTodoService } from '../../testing/todo.service.mock';
+import { Todo } from './todo';
+import { TodoListComponent } from './todo-list.component';
+import { TodoService } from './todo.service';
+
+const COMMON_IMPORTS: unknown[] = [
+  FormsModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatOptionModule,
+  MatButtonModule,
+  MatInputModule,
+  MatExpansionModule,
+  MatTooltipModule,
+  MatListModule,
+  MatDividerModule,
+  MatRadioModule,
+  MatSnackBarModule,
+  BrowserAnimationsModule,
+  RouterTestingModule,
+];
+
+describe('TodoListComponent', () => {
+  let todoList: TodoListComponent;
+  let fixture: ComponentFixture<TodoListComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [COMMON_IMPORTS, TodoListComponent],
+      providers: [{ provide: TodoService, useValue: new MockTodoService() }]
+    });
+  });
+
+  beforeEach(waitForAsync(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(TodoListComponent);
+      todoList = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  }));
+
+  it('contains all the todos', () => {
+    expect(todoList.serverFilteredTodos.length).toBe(3);
+  });
+
+  it('contains a user named "Chris"', () => {
+    expect(todoList.serverFilteredTodos.some((todo: Todo) => todo.owner === 'Chris')).toBe(true);
+  });
+
+  it('contains a todo with category "video games" ', () => {
+    expect(todoList.serverFilteredTodos.some((todo: Todo) => todo.category === 'video games')).toBe(true);
+  });
+
+  it('contains a todo with category "homework" ', () => {
+    expect(todoList.serverFilteredTodos.some((todo: Todo) => todo.category === 'homework')).toBe(true);
+  });
+
+  it('contains a todo with category "software design" ', () => {
+    expect(todoList.serverFilteredTodos.some((todo: Todo) => todo.category === 'software design')).toBe(true);
+  });
+
+  it('contains a todo with status "true" ', () => {
+    expect(todoList.serverFilteredTodos.some((todo: Todo) => todo.status === true )).toBe(true);
+  });
+
+  it('contains a todo with status "false" ', () => {
+    expect(todoList.serverFilteredTodos.some((todo: Todo) => todo.status === false )).toBe(true);
+  });
+
+  it('contains a todo with body "Ipsum esse est ullamco magna tempor anim laborum non officia deserunt veniam commodo. Aute minim incididunt ex commodo." ',
+  () => {
+    expect(todoList.serverFilteredTodos.some((todo: Todo) => todo.body === 'Ipsum esse est ullamco magna tempor anim laborum non officia deserunt veniam commodo. Aute minim incididunt ex commodo.')).toBe(true);
+  });
+});
+
+describe('Misbehaving Todo List', () => {
+  let todoList: TodoListComponent;
+  let fixture: ComponentFixture<TodoListComponent>;
+
+  let todoServiceStub: {
+    getTodos: () => Observable<Todo[]>;
+    getTodosFiltered: () => Observable<Todo[]>;
+  };
+
+  beforeEach(() => {
+    todoServiceStub = {
+      getTodos: () => new Observable(observer => {
+        observer.error('getTodos() Observer generates an error');
+      }),
+      getTodosFiltered: () => new Observable(observer => {
+        observer.error('getTodosFiltered() Observer generates an error');
+      })
+    };
+
+    TestBed.configureTestingModule({
+      imports: [COMMON_IMPORTS, TodoListComponent],
+      providers: [{ provide: TodoService, useValue: todoServiceStub }]
+});
+  });
+
+  beforeEach(waitForAsync(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(TodoListComponent);
+      todoList = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  }));
+
+  it('fails to load users if we do not set up a UserListService', () => {
+    expect(todoList.serverFilteredTodos).toBeUndefined();
+  });
+});
---
+++ client/src/app/todos/todo-list.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Todo } from './todo';
+import { TodoService } from './todo.service';
+import { Subject, takeUntil } from 'rxjs';
+import { RouterLink } from '@angular/router';
+import { MatNavList, MatListSubheaderCssMatStyler, MatListItem, MatListItemAvatar, MatListItemTitle, MatListItemLine } from '@angular/material/list';
+
+import { MatRadioGroup, MatRadioButton } from '@angular/material/radio';
+import { MatOption } from '@angular/material/core';
+import { MatSelect } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { MatInput } from '@angular/material/input';
+import { MatFormField, MatLabel, MatHint, MatError } from '@angular/material/form-field';
+import { MatCard, MatCardTitle, MatCardContent } from '@angular/material/card';
+
+
+@Component ({
+  selector: 'app-todo-list-component',
+  templateUrl: 'todo-list.component.html',
+    styleUrls: ['./todo-list.component.scss'],
+    providers: [],
+    standalone: true,
+    imports: [MatCard, MatCardTitle, MatCardContent, MatFormField, MatLabel, MatInput, FormsModule, MatHint,
+      MatSelect, MatOption, MatRadioGroup, MatRadioButton, MatNavList, MatListSubheaderCssMatStyler, MatListItem,
+       RouterLink, MatListItemAvatar, MatListItemTitle, MatListItemLine, MatError],
+})
+export class TodoListComponent implements OnInit, OnDestroy {
+  public serverFilteredTodos: Todo[];
+  public filteredTodos: Todo[];
+
+  public todoStatus: boolean;
+  public todoOwner: string;
+  public todoBody: string;
+  public todoCategory: string;
+
+
+  errMsg = '';
+  private ngUnsubscribe = new Subject<void>();
+
+    /**
+   * This constructor injects both an instance of `TodoService`
+   * and an instance of `MatSnackBar` into this component.
+   *
+   * @param todoService the `TodoService` used to get todos from the server
+   * @param snackBar the `MatSnackBar` used to display feedback
+   */
+  constructor(private todoService: TodoService, private snackBar: MatSnackBar) {
+  }
+
+  getTodosFromServer() {
+
+    this.todoService.getTodos({
+    }).pipe(
+      takeUntil(this.ngUnsubscribe)
+    ).subscribe({
+      next: (returnedTodos) => {
+          this.serverFilteredTodos = returnedTodos;
+          this.updateFilter();
+      },
+
+      error: (err) => {
+        if (err.error instanceof ErrorEvent) {
+          this.errMsg = `Problem in the client – Error: ${err.error.message}`;
+        } else {
+          this.errMsg = `Problem contacting the server – Error Code: ${err.status}\nMessage: ${err.message}`;
+        }
+      },
+    })
+  }
+
+  public updateFilter() {
+    this.filteredTodos = this.todoService.filterTodos(
+      this.serverFilteredTodos, {}
+    );
+  }
+
+  ngOnInit(): void {
+    this.getTodosFromServer();
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+}
+
+
